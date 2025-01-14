aaee415c7f194e4589544ea54288be4b4c35a4c4
fgeiger <florian.geiger21@gmail.com>
2025-01-14 18:29:07 +0100

    Refactor chart and Project Organization

---
+++ HealthKitAndCharts/Screens/DashboardView.swift
@@ -0,0 +1,81 @@
+import SwiftUI
+import Charts
+
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case steps, weight
+    var id: Self { self }
+    
+    var title: String {
+        switch self {
+        case .steps: return "Steps"
+        case .weight: return "Weight"
+        }
+    }
+}
+
+struct DashboardView: View {
+    @Environment(HealthKitManager.self) private var hkManager
+    @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
+    @State private var isShowPermissionPrimingSheet = false
+    @State private var selectedStat: HealthMetricContext = .steps
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    statPicker
+                    
+                    StepBarChart(selectedState: selectedStat, chartData: hkManager.stepData)
+                    
+                    averagesSection
+                }
+                .padding()
+            }
+            .task {
+                await hkManager.fetchStepCount()
+                isShowPermissionPrimingSheet = !hasSeenPermissionPriming
+            }
+            .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                HealthDataListView(metric: metric)
+            }
+            .sheet(isPresented: $isShowPermissionPrimingSheet) {
+                HealthKitPermissionPrimingView(hasSeen: $isShowPermissionPrimingSheet)
+            }
+        }
+        .tint(selectedStat == .steps ? .pink : .indigo)
+    }
+    
+    private var statPicker: some View {
+        Picker("Selected Stat", selection: $selectedStat) {
+            ForEach(HealthMetricContext.allCases) { metric in
+                Text(metric.title)
+            }
+        }
+        .pickerStyle(.segmented)
+    }
+
+    
+    private var averagesSection: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Label("Averages", systemImage: "calendar")
+                    .font(.title3.bold())
+                    .foregroundStyle(.pink)
+                
+                Text("Last 28 Days")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.secondary)
+                .frame(height: 240)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+    }
+}
+
+#Preview {
+    DashboardView().environment(HealthKitManager())
+}
