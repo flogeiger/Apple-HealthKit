0abda3ff9416a49f65f5bb531765a0c5d22d2898
fgeiger <florian.geiger21@gmail.com>
2025-01-25 23:38:34 +0100

    HealthKit Manager Documentation DocC

--- HealthKitAndCharts/Chart/Chart Utilites/ChartMath.swift
+++ HealthKitAndCharts/Chart/Chart Utilites/ChartMath.swift
@@ -9,6 +9,11 @@ import Foundation
 import Algorithms
 
 struct ChartMath {
+    /// Calculates the average value for each weekday from a given list of health metrics.
+    /// - Parameter metric: An array of `HealthMetric` objects containing date and value pairs.
+    /// - Returns: An array of `DateValueChartData` objects, where each object represents the average value for a specific weekday.
+    ///
+    /// The method groups the input metrics by weekday, calculates the average for each group, and returns the results as chart data.
     static func averageWeekdayCount(for metric: [HealthMetric]) -> [DateValueChartData]{
         let sortedByWeekday = metric.sorted(using: KeyPathComparator(\.date.weekdayInt))
         let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt ==  $1.date.weekdayInt}
@@ -26,6 +31,10 @@ struct ChartMath {
         return weekdayChartData
     }
     
+    /// Calculates the average daily weight differences for each weekday.
+    /// - Parameter weights: An array of ``HealthMetric`` objects containing daily weight measurements.
+    /// - Returns: An array of ``DateValueChartData`` objects, where each object represents the average daily weight difference for a specific weekday.
+    /// This method calculates the day-to-day difference in weight, groups the differences by weekday, and computes the average for each group.
     static func averageDailyWeightDiffs(for weights: [HealthMetric]) -> [DateValueChartData]{
         var diffValues: [(date:Date, value: Double)] = []
         
